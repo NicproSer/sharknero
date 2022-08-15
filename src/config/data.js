edafee91dca5e76e1f843c9d19a08dc6f24451b3
NICPRO SERVICE <84345977+NicproSer@users.noreply.github.com>
2022-08-15 16:58:00 -0400

    Blog

--- src/config/data.js
+++ src/config/data.js
@@ -4,16 +4,18 @@ export const blogList = [
     title: "¿Qué es un hosting y para qué sirve?",
     category: "Hosting",
     subCategory: ["Web", "Desarrollo Web"],
-    description:
-      "Lorem Ipsum Lkjhdedfjdcjdfoedoidd f fueokfjfdouockdhriujdivdoi u odifudkjdodoifdofi ",
-    image: "./assets/que-es-un-hosting.png",
+    description: [
+      "Un Hosting es un espacio en un servidor, que sirve para almacenar páginas web, conectado a la red para que los navegadores puedan acceder a un sitio. Esto lo convierte en un servicio fundamental si queremos tener nuestro propio blog, e-commerce o portal online.",
+    ],
+    image:
+      "https://s3.us-east-2.amazonaws.com/mgpanel/397-pexels-manuel-geissinger-325229.jpg",
     date: "2022-08-15",
   },
   {
     id: 2,
-    title: "Blog Post 2",
-    category: "Web Development",
-    subCategory: ["Vue", "JS", "Bootstrap"],
+    title: "¿Y para qué quiero una página web?",
+    category: "Desarrollo Web",
+    subCategory: ["Web", "", "Bootstrap"],
     description:
       "Lorem Ipsum Lkjhdedfjdcjdfoedoidd f fueokfjfdouockdhriujdivdoi u odifudkjdodoifdofi oidufoirufodjcd df",
     image: "https://source.unsplash.com/random",
