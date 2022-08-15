91c80bd244093b20ad91a40d67d4f1df21833666
NICPRO SERVICE <84345977+NicproSer@users.noreply.github.com>
2022-08-15 16:52:18 -0400

    sync

--- src/config/data.js
+++ src/config/data.js
@@ -1,13 +1,13 @@
 export const blogList = [
   {
     id: 1,
-    title: "Blog Post 1",
-    category: "Web Development",
-    subCategory: ["React", "NodeJS", "Express"],
+    title: "¿Qué es un hosting y para qué sirve?",
+    category: "Hosting",
+    subCategory: ["Web", "Desarrollo Web"],
     description:
       "Lorem Ipsum Lkjhdedfjdcjdfoedoidd f fueokfjfdouockdhriujdivdoi u odifudkjdodoifdofi ",
-    image: "https://source.unsplash.com/random",
-    date: "2020-01-01",
+    image: "./assets/que-es-un-hosting.png",
+    date: "2022-08-15",
   },
   {
     id: 2,
