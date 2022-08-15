677fb315dc2926f65ee796b51764c3eed514e700
NICPRO SERVICE <84345977+NicproSer@users.noreply.github.com>
2022-08-15 17:00:05 -0400

    blog

--- src/config/data.js
+++ src/config/data.js
@@ -6,6 +6,7 @@ export const blogList = [
     subCategory: ["Web", "Desarrollo Web"],
     description: [
       "Un Hosting es un espacio en un servidor, que sirve para almacenar páginas web, conectado a la red para que los navegadores puedan acceder a un sitio. Esto lo convierte en un servicio fundamental si queremos tener nuestro propio blog, e-commerce o portal online.",
+      "Dicho de otra manera, el hosting es como una habitación de hotel que nos permite hospedarnos en un lugar en específico. En este caso, tener un hosting nos da posibilidad de hospedar o alojar nuestra web no en un hotel sino en un servidor. Nuestra web puede contener: texto, imágenes, videos, archivos PDF y más.",
     ],
     image:
       "https://s3.us-east-2.amazonaws.com/mgpanel/397-pexels-manuel-geissinger-325229.jpg",
