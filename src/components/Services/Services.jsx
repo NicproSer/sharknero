f3841b7a61e83d1312d33df0ed6f3249cb55cca6
NICPRO SERVICE <84345977+NicproSer@users.noreply.github.com>
2022-08-15 16:01:34 -0400

    sync

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -8,13 +8,29 @@ class Services extends React.Component {
   render() {
     return (
       <>
-      <Preloader/>
-      <Navbar/>
-      <div id="servicios" className="container">
-      <h1>Servicios</h1>
-      </div>
-      <ScrollButton/>
-      <Footer/>
+        <Preloader />
+        <Navbar />
+        <div id="servicios" className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="text-center">Servicios</h1>
+            </div>
+          </div>
+          <div className="row mb-5">
+            <div className="col-lg-4">
+              <div className="card mb-5 shadow card-price">
+                <div className="card-header">
+                  <h2>Desde 125$</h2>
+                </div>
+                <div className="card-body">
+                  <h3 className="card-heading text-center"></h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <ScrollButton />
+        <Footer />
       </>
     );
   }
