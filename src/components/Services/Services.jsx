9de51b02e2a2acd7d6f6019f95f3d75d17c24f56
NICPRO SERVICE <84345977+NicproSer@users.noreply.github.com>
2022-08-16 16:34:45 -0400

    sync

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -43,7 +43,83 @@ class Services extends React.Component {
                 </div>
               </div>
             </div>
-            <div className="col-lg-4"></div>
+            <div className="col-lg-4">
+              <div className="card mb-5 shadow card-price">
+                <div className="card-header text-center">
+                  <h2>Aplicacion Web</h2>
+                  <h3>Desde 165$</h3>
+                </div>
+                <div className="card-body">
+                  <ul className="list-unstyled text-sm mb-4 pricing-feature-list">
+                    <li> Diseño e imagen de tu página web </li>
+                    <li> Imágenes de stock </li>
+                    <li> Adaptada a móviles y 100% responsive </li>
+                    <li> 1 o más idioma </li>
+                    <li> Autogestionable </li>
+                    <li> Información de la empresa </li>
+                    <li> Información de contacto </li>
+                    <li> Navegación interactiva </li>
+                    <li> Contenido de calidad </li>
+                    <li> Botones y ligas de redes sociales </li>
+                  </ul>
+                  <div className="text-center">
+                    <a href="/cuenta" className="btn btn-primary">
+                      Solicitar <i class="bx bx-cart"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="card mb-5 shadow card-price">
+                <div className="card-header text-center">
+                  <h2>Aplicacion Móvil</h2>
+                  <h3>Proximamente....</h3>
+                </div>
+                <div className="card-body">
+                  <ul className="list-unstyled text-sm mb-4 pricing-feature-list">
+                    <li>
+                      <br />
+                    </li>
+                    <li>
+                      <br />
+                    </li>
+                    <li>
+                      <br />
+                    </li>
+                    <li>
+                      <br />
+                    </li>
+                    <li>
+                      <br />
+                    </li>
+                    <li>
+                      <br />
+                    </li>
+                    <li>
+                      <br />
+                    </li>
+                    <li>
+                      <br />
+                    </li>
+                    <li>
+                      <br />
+                    </li>
+                    <li>
+                      <br />
+                    </li>
+                    <li>
+                      <br />
+                    </li>
+                  </ul>
+                  <div className="text-center">
+                    <a href="/cuenta" className="btn btn-primary" disabled>
+                      Solicitar <i class="bx bx-cart"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <ScrollButton />
